eb79313ea2d7aa5383c14633fb2d19958d90ebd7
Vanessa Wingårdh <vwingardh@gmail.com>
2023-04-22 11:08:10 +0200

    Committing HTML/CSS/JS

---
+++ src/main/java/com/addressBook/website/WebsiteController.java
@@ -0,0 +1,13 @@
+package com.addressBook.website;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebsiteController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
