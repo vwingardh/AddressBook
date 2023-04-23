c567ed20c8f59db27e3e73d4c377cd0d576075d9
Vanessa Wingårdh <vwingardh@gmail.com>
2023-04-23 11:23:56 +0200

    Initial version of getAddresses()

--- src/main/resources/static/js/function.js
+++ src/main/resources/static/js/function.js
@@ -1,1 +1,22 @@
-alert("Test");
+function getAddresses(element) {
+    fetch("http://localhost:8080/addresses")
+        .then((response) => response.json())
+        .then((data) => {
+            const addressList = document.createElement("div");
+
+            for (let i = 0; i < data.length; i++) {
+                const address = data[i];
+                const addressDiv = document.createElement("div");
+                addressDiv.textContent = `${address.firstName} ${address.lastName}, ${address.addressLine1}, ${address.addressLine2}, ${address.postalCode}, ${address.city}, ${address.country}`;
+                addressList.appendChild(addressDiv);
+            }
+            element.appendChild(addressList);
+        })
+        .catch((error) => console.error(error));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const mainElement = document.querySelector("main");
+    getAddresses(mainElement);
+});
+
